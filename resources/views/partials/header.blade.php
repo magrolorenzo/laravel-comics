c58dccd14efee10c85648cbed979b7686b509b1f
Lorenzo Magro <magrolorenzo@gmail.com>
2021-01-07 18:51:16 +0100

    Stilato main menu, sembra non andare il box-sizing border box

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,47 @@
 <header>
-        <div class="header-top">
-            <div class="container">
-                <span>DC Power Visa</span>
-                <span>Additional DC Sites </span>
-            </div>
+    {{-- Header Top --}}
+    <div class="header-top">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li><a href="#" class="uppercase" >DC Power℠ Visa®</a></li>
+                    <li><a href="#" class="uppercase" >Additional DC Sites</a></li>
+                </ul>
+            </nav>
         </div>
-        <div class="header-bottom">
-            <div class="container">
+    </div>
+    {{-- Header Bottom --}}
+    <div class="header-bottom">
+        <div class="container">
 
+            <div class="navbar">
+
+                {{-- DC Logo --}}
+                <div class="logo-container">
+                    <img src="{{ asset('images/dc-logo.png') }}" alt="dc-logo">
+                </div>
+
+                {{-- Main nav menu --}}
+                <div class="menu">
+                    <ul>
+                        <li> <a href="#" class="uppercase">Characters</a></li>
+                        <li> <a href="#" class="uppercase">Comics</a></li>
+                        <li> <a href="#" class="uppercase">Movies</a></li>
+                        <li> <a href="#" class="uppercase">Tv</a></li>
+                        <li> <a href="#" class="uppercase">Games</a></li>
+                        <li> <a href="#" class="uppercase">Collectibles</a></li>
+                        <li> <a href="#" class="uppercase">Videos</a></li>
+                        <li> <a href="#" class="uppercase">Fans</a></li>
+                        <li> <a href="#" class="uppercase">News</a></li>
+                        <li> <a href="#" class="uppercase">Shop</a></li>
+                    </ul>
+                </div>
+
+                {{-- Search bar --}}
+                <div class="search-container">
+
+                </div>
             </div>
         </div>
+    </div>
 </header>
