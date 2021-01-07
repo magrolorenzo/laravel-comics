c34da06e85683e8e75ac3680cbc7eb1e0b8b1a2b
Lorenzo Magro <magrolorenzo@gmail.com>
2021-01-07 16:22:55 +0100

    creazione progetto

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.app")
+
+@section("page-title", "Homepage Laravel DC Comics")
+@section("content")
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.app")
+
+@section("content")
+    <header>
+        <div class="header-top">
+            {{-- 30px --}}
+        </div>
+        <div class="header-bottom">
+            {{-- 110px --}}
+        </div>
+    </header>
+@endsection
