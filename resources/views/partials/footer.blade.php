5f56b95d99e5b9b840b29cdc623793847fc10dea
Lorenzo Magro <magrolorenzo@gmail.com>
2021-01-10 16:51:57 +0100

    aggiunto bg footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="top-footer">
+        <div class="container">
+
+        </div>
+    </div>
+    <div class="bottom-footer">
+        <div class="container">
+
+        </div>
+    </div>
+</footer>
